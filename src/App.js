d0650b9edbbfcd13d5372c088da737f4d25f13e1
Eric Hussmann <eric@vogey.com>
2018-10-31 20:16:35 -0700

    Hydrating timeline from context state

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
       },
       {
         name: 'C',
-        machineId: 2,
+        machineId: 1,
         startDay: 3,
         duration: 48,
         engineers: 5
@@ -44,74 +44,6 @@ class App extends Component {
 
 
   render() {
-
-    // const weekStart = moment().startOf('day');
-    // const weekEnd = moment(weekStart).add(7, 'days');
-    // const customTimes = {
-    //   scrubber: moment() 
-    // };
-    // const options = {
-    //   width: '90%',
-    //   // height: '300px',
-    //   stack: true,
-    //   showMajorLabels: true,
-    //   showCurrentTime: false,
-    //   start: weekStart,
-    //   end: weekEnd,
-    //   zoomMin: 1000000,
-    //   type: 'range',
-    //   editable: true,
-    //   format: {
-    //     minorLabels: {
-    //       minute: 'h:mma',
-    //       hour: 'ha'
-    //     }
-    //   }
-    // }
-
-    // const groups = [
-    //   {
-    //     id: 1,
-    //     content: 'Machine 1',
-    //   },
-    //   {
-    //     id: 2,
-    //     content: 'Machine 2'
-    //   },
-    //   {
-    //     id: 3,
-    //     content: 'Machine 3'
-    //   }
-    // ];
-
-    // const items = [
-    //   {
-    //     start: weekStart,
-    //     end: moment(weekStart).add(12, 'h'),
-    //     content: 'Service Action A',
-    //     group: 1,
-    //     editable:true
-    //   },
-    //   {
-    //     start: moment(weekStart).add(1, 'day'),//new Date(2010, 7, 15),
-    //     end: moment(weekStart).add(1, 'day').add(24, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    //     content: 'Service Action B',
-    //     group: 2
-    //   },
-    //   {
-    //     start: moment(weekStart).add(3, 'day'),//new Date(2010, 7, 15),
-    //     end: moment(weekStart).add(3, 'day').add(48, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    //     content: 'Service Action C',
-    //     group: 1
-    //   },
-    //   {
-    //     start: moment(weekStart).add(4, 'day'),//new Date(2010, 7, 15),
-    //     end: moment(weekStart).add(4, 'day').add(12, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    //     content: 'Service Action D',
-    //     group: 2
-    //   }
-    // ];
-
     return (
       <div className="App">
         <PlannerContext.Provider value={this.state}>
--- src/PlannerTimeline.component.js
+++ src/PlannerTimeline.component.js
@@ -4,77 +4,61 @@ import moment from 'moment';
 import {PlannerContext, PlannerConsumer} from './Planner.context';
 
 
-const weekStart = moment().startOf('day');
-const weekEnd = moment(weekStart).add(7, 'days');
-const customTimes = {
-  scrubber: moment() 
-};
-const options = {
-  width: '90%',
-  // height: '300px',
-  stack: true,
-  showMajorLabels: true,
-  showCurrentTime: false,
-  start: weekStart,
-  end: weekEnd,
-  zoomMin: 1000000,
-  type: 'range',
-  editable: true,
-  format: {
-    minorLabels: {
-      minute: 'h:mma',
-      hour: 'ha'
-    }
-  }
-}
-
-const groups = [
-  {
-    id: 1,
-    content: 'Machine 1',
-  },
-  {
-    id: 2,
-    content: 'Machine 2'
-  },
-  {
-    id: 3,
-    content: 'Machine 3'
-  }
-];
-
-const items = [
-  {
-    start: weekStart,
-    end: moment(weekStart).add(12, 'h'),
-    content: 'Service Action A',
-    group: 1,
-    editable:true
-  },
-  {
-    start: moment(weekStart).add(1, 'day'),//new Date(2010, 7, 15),
-    end: moment(weekStart).add(1, 'day').add(24, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    content: 'Service Action B',
-    group: 2
-  },
-  {
-    start: moment(weekStart).add(3, 'day'),//new Date(2010, 7, 15),
-    end: moment(weekStart).add(3, 'day').add(48, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    content: 'Service Action C',
-    group: 1
-  },
-  {
-    start: moment(weekStart).add(4, 'day'),//new Date(2010, 7, 15),
-    end: moment(weekStart).add(4, 'day').add(12, 'hours'),//new Date(2010, 8, 2),  // end is optional
-    content: 'Service Action D',
-    group: 2
-  }
-];
-
 const PlannerTimeline = (props) => (
 
 	<PlannerConsumer>
 		{context=> {
+      const weekStart = moment().startOf('day');
+      const weekEnd = moment(weekStart).add(7, 'days');
+      const customTimes = {
+        scrubber: moment() 
+      };
+      const options = {
+        width: '90%',
+        // height: '300px',
+        stack: true,
+        showMajorLabels: true,
+        showCurrentTime: false,
+        showTooltips: true,
+        start: weekStart,
+        end: weekEnd,
+        zoomMin: 1000000,
+        type: 'range',
+        editable: false,
+        format: {
+          minorLabels: {
+            minute: 'h:mma',
+            hour: 'ha'
+          }
+        }
+      }
+			const groups = [
+			  {
+			    id: 1,
+			    content: 'Machine 1',
+			  },
+			  {
+			    id: 2,
+			    content: 'Machine 2'
+			  },
+			  {
+			    id: 3,
+			    content: 'Machine 3'
+			  }
+			];
+
+      const items = context.serviceActions.map((action) => {
+        let start = moment(weekStart).add(action.startDay, 'days');
+        let end = moment(start).add(action.duration, 'hours');
+        return  {
+          start: start,
+          end: end,
+          content: 'sa.'+action.name,
+          group: action.machineId,
+          title: 'Engineers: '+action.engineers
+        }
+      });
+
 			return(
 				<div>
 				<h4>Total Engineers: {context.totalEngineers}</h4>
